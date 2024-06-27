b835ffb12a5a5339345e5c5c61e9fc3b5b4bde1f
md-alramlawi <md.alramlawi@gmail.com>
2024-06-27 14:20:33 +0300

    Initial commit

---
+++ shared/src/androidMain/kotlin/com/alramlawi/pokemon/Platform.android.kt
@@ -0,0 +1,7 @@
+package com.alramlawi.pokemon
+
+class AndroidPlatform : Platform {
+    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
+}
+
+actual fun getPlatform(): Platform = AndroidPlatform()
