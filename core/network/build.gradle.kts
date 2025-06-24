fed38c207f66e59f31b18e5140cbd925ece601e8
md-alramlawi <md.alramlawi@gmail.com>
2025-06-24 18:26:02 +0300

    Gradle: Clean-up libs.versions

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
-    alias(libs.plugins.kotlinx.serialization)
+    alias(libs.plugins.kotlinxSerialization)
 }
 
 kotlin {
